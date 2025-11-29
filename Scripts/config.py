18531698e404928621bfea288e96c1bc4c7ad964
Abel Tesfa <abeltesfa198@gmail.com>
2025-11-29 08:20:48 +0300

    Task-1: Essential Configurations complete

---
+++ Scripts/config.py
@@ -0,0 +1,33 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+# Configuration for bank app IDs and names
+APP_IDS = {
+	"CBE": os.getenv("CBE_ID"),
+	"BOA": os.getenv("BOA_ID"),
+	"Dashen": os.getenv("Dashen_Id")
+}
+
+Bank_NAMES = {"CBE": "Commercial Bank of Ethiopia", 
+              "BOA": "Bank of Abyssinia", 
+              "Dashen": "Dashen Bank"}
+
+# Configuration for scraping parameters
+SCRAPING_CONFIG = {
+    "reviews_per_bank": os.getenv("REVIEWS_PER_BANK", 400),
+    "max_retries": os.getenv("MAX_RETRIES", 3),
+    "language": os.getenv("LANGUAGE", "en"),
+    "country": os.getenv("COUNTRY", "Ethiopia"),
+}
+
+# Configuration for data file paths
+DATA_PATHS = {
+    "raw": "data/raw/",
+    "processed": "data/processed/",
+    "raw_reviews": "data/raw/raw_reviews.csv",
+    "processed_reviews": "data/processed/processed_reviews.csv",
+    "sentiment_results": "data/processed/reviews_with_sentiment.csv",
+    "final_result": "data/processed/reviews_final.csv"
+}
